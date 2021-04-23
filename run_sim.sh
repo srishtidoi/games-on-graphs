547d65b561cf8f735f299965f3ec7c6403153027
srishtidoi <srishpat@gmail.com>
2021-04-23 07:57:50 +0530

    no longer sends output to text file

--- run_sim.sh
+++ run_sim.sh
@@ -2,14 +2,13 @@
 
 #### parameters
 
-note=moreq
+note=rep
 population=100
-eps=20
-network=smallworld
-rule=bayesian
+eps=100
+network=scalefree
+rule=reputation
 
 #### make and cd to new directory
-#### add comments at the end (more_r, more_t, etc.)
 
 dirname=$network"_"$rule"_n"$population"e"$eps"_"$note
 mkdir $dirname
@@ -17,14 +16,16 @@ cd $dirname
 mkdir outputs
 
 #### run the simulation
-python3 ../main.py $population $eps $network $rule | tee outputs/zoutput.txt
+python3 ../main.py $population $eps $network $rule 
+
+#| tee outputs/zoutput.txt
 
 ### split output file
-cd outputs
-split -l 1000000 -d --additional-suffix=.txt zoutput.txt file
+#cd outputs
+#split -l 1000000 -d --additional-suffix=.txt zoutput.txt file
 
 ### plot the graph
-cd ..
+#cd ..
 python3 ../plotting/plot_indir.py $eps save
 
 
