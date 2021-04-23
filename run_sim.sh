2aaf0f5821de633fb6df68fac1ebf6d020f5eff4
srishtidoi <srishpat@gmail.com>
2021-04-23 16:00:59 +0530

    new argument - out (to determine output). Added a few commments

--- run_sim.sh
+++ run_sim.sh
@@ -1,12 +1,18 @@
 #! /bin/sh
 
-#### parameters
+#### parameters ----->
+#### note: note for this particular sim (test, newmodel, p0.7, etc)
+#### eps: number of runs
+#### network: scalefree, smallworld, lattice, RR (random regular)
+#### rule: learning rule - imitate, bayesian, reputation
+#### out: kind of output - null, rep (for time evolution of reputation parameter) 
 
-note=rep
+note=test
 population=100
-eps=100
+eps=2
 network=scalefree
 rule=reputation
+out=null
 
 #### make and cd to new directory
 
@@ -16,11 +22,10 @@ cd $dirname
 mkdir outputs
 
 #### run the simulation
-python3 ../main.py $population $eps $network $rule 
+python3 ../main.py $population $eps $network $rule $out
 
+#### add this to direct the print output to a text file and then split it
 #| tee outputs/zoutput.txt
-
-### split output file
 #cd outputs
 #split -l 1000000 -d --additional-suffix=.txt zoutput.txt file
 
