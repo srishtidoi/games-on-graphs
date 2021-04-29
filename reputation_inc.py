88c2dc24102c660c90ee625e5243db87b3fdd4f5
srishtidoi <srishpat@gmail.com>
2021-04-30 05:05:34 +0530

    plots for new model of rep with public info

--- reputation_inc.py
+++ reputation_inc.py
@@ -2,16 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0,1,100)
-rep = x*(2-x)
+#rep1 = x*((2-x)**0.1)
+rep2 = x*((2-x)**0.2)
 
 #plt.xlim([-0.5, 0.5])
 #plt.ylim([-0.2, 1.2])
 plt.grid(True)
 plt.axhline(y=0, color='k')
 #plt.axvline(x=0, color='k')
-plt.ylabel('Repuation after increase due to cooperation')
-plt.xlabel('Repuation before increase')
+label2=('factor = 0.2')
+label=('factor $\leq 0$')
 
-plt.plot(x, rep, 'r-')
-plt.plot(x, x, 'k--', linewidth=0.5)
+plt.xlabel('P(C) before update')
+plt.ylabel('P(C) after update')
+#plt.plot(x, rep1, 'b-', label=label1)
+plt.plot(x, rep2, 'b-', label=label2)
+plt.plot(x, x, 'k--', linewidth=0.5, label=label)
+plt.legend()
 plt.show()
